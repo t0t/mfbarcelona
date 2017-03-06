fea8e7d980da6387b78a6b0ccc43573d91da6bd0
Sergio Forés Raga <sergiofores@gmail.com>
2017-03-06 18:54:26 +0100

    Init :rabbit2:

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
+// import "component";
 
+let q = 99;
+const ww = 12;
+console.log(q);
+console.log(ww);
---
+++ js/build.js
@@ -0,0 +1,1 @@
+!function(e,o){if("function"==typeof define&&define.amd)define([],o);else if("undefined"!=typeof exports)o();else{var n={exports:{}};o(),e.app=n.exports}}(this,function(){"use strict";var e=99,o=12;console.log(e),console.log(o)});
